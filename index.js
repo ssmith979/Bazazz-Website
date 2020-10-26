d3639a067f53bcd2059e54295e9e3864ebae7b9e
Susanna Smith <50603908+ssmith979@users.noreply.github.com>
2020-10-26 13:11:38 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var vid = document.getElementById("vid");
+function autoplay() { 
+  vid.autoplay = true;
+  vid.load();
+}
+
+const constraints = {
+    name: {
+        presence: { allowEmpty: false }
+    },
+    email: {
+        presence: { allowEmpty: false },
+        email: true
+    },
+    message: {
+        presence: { allowEmpty: false }
+    }
+};
+
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function (event) {
+  const formValues = {
+      name: form.elements.name.value,
+      email: form.elements.email.value,
+      message: form.elements.message.value
+  };
+
+  const errors = validate(formValues, constraints);
+
+  if (errors) {
+    event.preventDefault();
+    const errorMessage = Object
+        .values(errors)
+        .map(function (fieldValues) { return fieldValues.join(', ')})
+        .join("\n");
+
+    alert(errorMessage);
+  }
+}, false);
+
---
+++ submitted.php
@@ -0,0 +1,181 @@
+<?php 
+ini_set('display_errors', 1 );
+error_reporting(E_ALL);
+if(isset($_POST['submit'])) {
+    $from = "poductions@bazazz.com";
+    $to = "bazazzproductions@gmail.com";
+    $name = ($_POST['name']);
+    $email = ($_POST['email']);
+    $message = ($_POST['message']);
+    $subject = "Checking PHP mail";
+    $headers = "From:" . $from;
+
+    $bodyParagraphs = ["Name: {$name}", "Email: {$email}", "Message:", $message];
+    $body = join(PHP_EOL, $bodyParagraphs);
+
+    if (mail($to, $subject, $body, $headers)) {
+       $success = "<p id='success'> Your message was sent!</p>";
+    } else {
+        $errorMessage = 'Oops, something went wrong. Please try again later';
+    }
+ }
+
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-TVFJLFX');</script>
+    <!-- End Google Tag Manager -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@700&family=Peddana&family=Playfair+Display:ital,wght@1,600&display=swap" rel="stylesheet">
+    <link rel='stylesheet' href='https://bootswatch.com/4/lux/bootstrap.css'>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"> <script src='index.js'></script>
+    <link rel='stylesheet' href='CSS/styles.css' type='text/css' <?php echo time(); ?>>
+  
+</head>
+
+<body onload="autoplay()" >
+    <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVFJLFX"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    <?php include_once("analyticstracking.php") ?>
+ 
+    <div class="container-fluid">
+
+        <div class='fill ' id='row-margin'  max-width="100%" max-height='auto' style="background-image: url('IMG/flecks8.png');">
+            <img src='IMG/logo3.png' height="auto" width='50%' id='logo-lg' class= "visible-lg-block hidden-xl hidden-md hidden-sm hidden-xs">
+            <img src='IMG/logo3.png' height="auto" width='50%' id='logo-md' class= "visible-md-block hidden-xl hidden-lg  hidden-sm hidden-xs">
+            <img src='IMG/logo3.png' height="auto" width='50%' id='logo-sm' class= "visible-sm-block hidden-xl hidden-lg hidden-md hidden-xs">
+            <img src='IMG/logo3.png' height="auto" width='50%' id='logo-xs' class= "visible-xs-block hidden-xl hidden-lg hidden-md hidden-sm">
+
+            <!-- Home Page -->
+        
+            <div class='row '>
+                <div class='bozazz-div col-lg-12 col-md-12 col-sm-12 col-12' id='bozazz-div'>
+                    <img id='bazazz-text' src='IMG/bazazzText2.png' >
+                    <h1 id='about-lg' class='visible-lg-block hidden-md hidden-sm hidden-xs' > Entertainment that fuses vaudevillian comedy and the Great American Songbook</h1>
+                    <h1 id='about-md' class='visible-md-block hidden-lg hidden-sm hidden-xs' > Entertainment that fuses vaudevillian comedy and the Great American Songbook</h1>
+                    <h1 id='about-sm' class='visible-sm-block hidden-xs hidden-lg hidden-md' > Entertainment that fuses vaudevillian comedy and the Great American Songbook</h1>
+                    <h1 id='about-xs' class='visible-xs-block hidden-sm hidden-lg hidden-md' > Entertainment that fuses vaudevillian comedy and the Great American Songbook</h1>
+                </div> 
+            
+                <div class='col-lg-12 col-md-12 col-sm-12 col-12' height="70pt">
+                    <h2 class='text-align2 visible-lg-block visible-md-block hidden-sm hidden-xs' id='currentshow-text-lg'> Currently Touring</h2>
+                    <h2 class='text-align2 visible-sm-block visible-xs-block hidden-lg hidden-md' id='currentshow-text-sm'> Currently Touring</h2>
+                </div>
+            
+                <div class='col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12' >
+
+                    <iframe id='vid' width="560" src="https://www.youtube.com/embed/2yKVkfSYo4o?autoplay=1" frameborder="0" allow="accelerometer; autoplay; muted; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <!--
+                    <script>
+                        document.getElementById('vid').play();
+                    </script>
+                    -->
+                </div>
+
+                <svg width="4%" height="4%" viewBox="0 0 16 16" class="bi bi-chevron-down down1 arrow bounce" fill="white" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                </svg>
+
+            </div>
+
+
+            <br><br><br><br><br>
+
+            <!-- Show Synopsis Page-->
+
+                
+            <div class="row">
+                <div class='col-lg-12 col-md-12 col-sm-12 col-12' height="70pt">
+                    <h1 id='shows-text-lg' class='visible-lg-block hidden-md hidden-sm hidden-xs'> Shows</h1>
+                    <h1 id='shows-text-md'class=' visible-md-block hidden-lg hidden-sm hidden-xs'> Shows</h1>
+                    <h1 id='shows-text-s' class='visible-sm-block visible-xs-block hidden-md hidden-lg'> Shows</h1>
+                </div>
+
+                <!--First Show Tab-->
+                <div class='container container-background'>
+                    <div class='col-lg-12 col-md-12 col-sm-12 col-12' height="70pt">
+                        <h1 class='title-txt-lg visible-lg-block hidden-md hidden-sm hidden-xs'> My Way</h1>
+                        <h1 class='title-txt visible-sm-block visible-md-block visible-xs-block hidden-lg'> My Way</h1>
+                        <p id='synopsis-text-lg' class='visible-lg-block hidden-md hidden-sm hidden-xs'> Bazazz Productions’ inaugural show is a one-woman ode to Old Hollywood. The songs, dance numbers and vaudeville sketches that we remember with such nostalgia are making a comeback. With the heart of Judy Garland and the spirit of Carol Burnett, Whitney Grace and our 4-piece band create an experience that you’ll never forget.  </p>
+                        <p id='synopsis-text'class='visible-sm-block visible-md-block visible-xs-block hidden-lg'> Bazazz Productions’ inaugural show is a one-woman ode to Old Hollywood. The songs, dance numbers and vaudeville sketches that we remember with such nostalgia are making a comeback. With the heart of Judy Garland and the spirit of Carol Burnett, Whitney Grace and our 4-piece band create an experience that you’ll never forget.  </p>
+                        
+                    </div>
+                    <img src='IMG/newpaint1.png' height='auto' width="900pt" class='poster-lg visible-lg-block hidden-md hidden-sm hidden-xs'>
+                    <img src='IMG/newpaint1.png' height='auto' width="900pt" class='poster-md visible-md-block hidden-lg hidden-sm hidden-xs'>
+                    <img src='IMG/newpaint1.png' height='auto' width="900pt" class='poster-sm visible-sm-block hidden-xs- hidden-lg hidden-md'>
+                    <img src='IMG/newpaint1.png' height='auto' width="900pt" class='poster-xs visible-xs-block hidden-sm hidden-lg hidden-md'>
+                </div>
+
+                <!--Second Show Tab-->
+                <div class='container container-background'>
+                    <div class='col-lg-12 col-md-12 col-sm-12 col-12' height="70pt">
+                        <h1 class='title-txt2-lg visible-lg-block hidden-md hidden-sm hidden-xs'> Merry Christmas <span style="font-family: 'Playfair Display', serif; color: red"> Dahling </span></h1>
+                        <h1 class='title-txt2 visible-sm-block visible-md-block visible-xs-block hidden-lg'> Merry Christmas <span style="font-family: 'Playfair Display', serif; color: red"> Dahling </span></h1>
+                        <p id='synopsis-text-lg' class='visible-lg-block hidden-md hidden-sm hidden-xs'> Bazazz Productions is proud to announce its first seasonal show. ‘Merry Christmas, Dahling’ really brings out our Bette Davis side. Hear your favorite Holiday songs, in our signature ‘40s style, and laugh along to these fantastic Kay Thompson dance numbers. We guarantee you’ll leave with a renewed enthusiasm for the season! </p>
+                        <p id='synopsis-text' class='visible-sm-block visible-md-block visible-xs-block hidden-lg'> Bazazz Productions is proud to announce its first seasonal show. ‘Merry Christmas, Dahling’ really brings out our Bette Davis side. Hear your favorite Holiday songs, in our signature ‘40s style, and laugh along to these fantastic Kay Thompson dance numbers. We guarantee you’ll leave with a renewed enthusiasm for the season! </p>
+                        
+                    </div>
+                    <img src='IMG/poster2.png' height='auto' width="900pt" class='poster-lg2 visible-lg-block hidden-md hidden-sm hidden-xs'>
+                    <img src='IMG/poster2.png' height='auto' width="900pt" class='poster-md2 visible-md-block hidden-lg hidden-sm hidden-xs'>
+                    <img src='IMG/poster2.png' height='auto' width="900pt" class='poster-sm2 visible-sm-block hidden-xs- hidden-lg hidden-md'>
+                    <img src='IMG/poster2.png' height='auto' width="900pt" class='poster-xs2 visible-xs-block hidden-sm hidden-lg hidden-md'>
+                </div>
+            </div>
+                
+            <br><br><br><br><hr style='background-color: rgba(62, 62, 66, 0.863);'><br><br><br><br><br>
+
+            <!-- Booking Page-->
+
+            <h1 id='booking-text'>Booking</h1>
+            <div class='container booking-container'> 
+            <h3 id='contact-text'> Contact us for booking dates & show details </h3>
+            <form class='form-margin' method='post' action='submitted.php' id='contact-form'>
+                <?php echo $success ?>
+                <div class='row'>
+                    <div class="form-group col name-div">
+                        <label class="form-font" for="name">Name</label>
+                        <input name='name' type="name" class="form-control" id="name" placeholder="name">
+                    </div>
+                    <div class="form-group col email-div"  width='60'>
+                        <label class="form-font" for="email">Email</label>
+                        <input name='email' type="email" class="form-control" id="email" placeholder="name@example.com" width='50%'>
+                    </div>
+                </div>
+
+                <div class="form-group message-div">
+                    <label class="form-font" for="message">Message</label>
+                    <textarea name='message' class="form-control" id="message" rows="8"></textarea>
+                </div>
+
+                <button name='submit' type='submit' value='send' class='btn btn-warning'> Submit</button>
+            </form>
+
+                <p class="form-font" id='contact-email'>Or Contact us via <a href='mailto: email@email.com'> bazazzproductions@gmail.com</a></p>
+            </div>
+
+            <br><br><br><br><br><br><br><br><br><br> 
+
+            <div id='footer-div'>
+                <hr  style='background-color: rgba(62, 62, 66, 0.863);'>
+                <p id='footer' style='color:aliceblue'> &copy; Copyright 2020 </p>   
+                <br><br><br><br><br><br><br><br>
+            </div>
+        
+        </div>
+
+    </div>
+
+</body>
+
+</html>
